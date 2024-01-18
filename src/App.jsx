2865fc87449737416a19b67be697d64d88a92fb9
Sasindumarasinghe1221 <sasindumarasinghe74@gmail.com>
2024-01-18 20:20:53 +0530

    varibles & functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
 
 
 function App() {
+  const firstname = "Jone";
+  const middlename = "Alan";
+  const surename = "Smith";
+  const age = "30";
+  const country = "Sri Lanka";
+  const getFullName = (f,m,s) => {
+    return  `${f} ${m} ${s}`;
+  }
+  const arr = ["apple","grapes","oranges"];
+  const lang =      <ul>
+  <li>HTML</li>
+  <li>CSS</li>
+  <li>JS</li>
+  <li>React</li>
+ </ul>;
+ const pobj = {
+  fname : "smith",
+  age : "60"
+ }
   return (
-    <div>
-    <h1 className="heading"> React sasi</h1>
-    <p>Grammarly’s free sentence checker allows you to
-       write your best wherever you love to write. Check for
-        run-on sentences, tone, clarity, and more with Grammarly.
-
-    </p>
+    <>
+    <div className="cont">
+       <h1>Employee details</h1>
+       <p>Full name:{firstname} {middlename} {surename}</p>
+       <p>Full name function:{getFullName("sasindu","sachindra","marasinghe")}</p>
+       <p>{firstname} likes to eat {arr [0]}</p>
+       <p>Age :{age}</p>
+       <p>country : {country}</p>
+       
+       <p>Frontend languages</p>
+   <p>{lang}</p>
+   <p>{pobj.fname} is {pobj.age} years old</p>
     </div>
+    </>
   )
 }
 
