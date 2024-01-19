979a5f4ecef8000dd5c4a9fe3f415d9dd570fa05
Sasindumarasinghe1221 <sasindumarasinghe74@gmail.com>
2024-01-19 11:41:19 +0530

    components & sub folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,23 @@
+import BodyContent from "./Components/BodyContent/BodyContent"
+import FooterContent from "./Components/FooterContent/FooterContent"
+import HeaderContent from "./Components/HeaderContent/HeaderContent"
 
 
 function App() {
-  const firstname = "Jone";
-  const middlename = "Alan";
-  const surename = "Smith";
-  const age = "30";
-  const country = "Sri Lanka";
-  const getFullName = (f,m,s) => {
-    return  `${f} ${m} ${s}`;
-  }
-  const arr = ["apple","grapes","oranges"];
-  const lang =      <ul>
-  <li>HTML</li>
-  <li>CSS</li>
-  <li>JS</li>
-  <li>React</li>
- </ul>;
- const pobj = {
-  fname : "smith",
-  age : "60"
- }
+
   return (
     <>
-    <div className="cont">
-       <h1>Employee details</h1>
-       <p>Full name:{firstname} {middlename} {surename}</p>
-       <p>Full name function:{getFullName("sasindu","sachindra","marasinghe")}</p>
-       <p>{firstname} likes to eat {arr [0]}</p>
-       <p>Age :{age}</p>
-       <p>country : {country}</p>
-       
-       <p>Frontend languages</p>
-   <p>{lang}</p>
-   <p>{pobj.fname} is {pobj.age} years old</p>
+    <div id="wrapper">
+    <HeaderContent/>
+    <BodyContent/>
+    <FooterContent/>
+    
+ 
     </div>
+      
+
     </>
+    
   )
 }
 
---
+++ src/Components/FooterContent/FooterContent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './FooterContent.css'
+
+
+function FooterContent() {
+  return (
+    <div id="footercontent">
+    <p>Footer content</p>
+  </div>
+  )
+}
+
+export default FooterContent
