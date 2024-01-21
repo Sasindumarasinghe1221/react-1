304cc25c1391b5a7e43d824edf610efbc4a31029
Sasindumarasinghe1221 <sasindumarasinghe74@gmail.com>
2024-01-21 11:03:30 +0530

    using parametrs & functions &event handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import HeaderContent from "./Components/HeaderContent/HeaderContent"
 
 
 function App() {
 
+  function hello(e){
+    console.log("sasi 1" , e.target);
+  }
+  //arraw function 
+  const hello2 = (e) => {
+    console.log("marasinghe", e);
+
+  }
+//using paramites
+function hello3(name,e){
+  console.log("hello 3" + name,e);
+}
+
+const hello4 = (name,e) => {
+  console.log("hello 4" + name,e);
+}
   return (
     <>
     <div id="wrapper">
-    <HeaderContent/>
+      <button onClick={hello}>click 1</button>
+      <br />
+      <button onClick={hello2}>click 2</button>
+      <br />
+      
+      <button onClick={(e) => {hello3( "sachindra",e)}}>click 3</button> <br />
+      <button onClick={(e) => {hello4( "mala",e.target)}}>click 4</button>
+
+
     
     </div>
       
