4844211b65463ac109be17ba285166493eb1dde1
Sasindumarasinghe1221 <sasindumarasinghe74@gmail.com>
2024-01-24 14:10:42 +0530

    Usestate & reactHook

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
 
+import { useState } from 'react';
 import './App.css';
-import HeaderContent from './Components/HeaderContent/HeaderContent';
-import BodyContent from './Components/BodyContent/BodyContent';
 function App() {
+//let num1 = 2;
+//value ekak dinamicaly venas krnva nm meka bavitha kranna mekata kynne reacthook kyla
+const [num1,setnum1] = useState(2)
+const [count,setcount] = useState(2)
 
+
+function handleClick(){
+  //num1 = 3;
+  setnum1(3)
+  //alert(num1)
+}
   
+const decrementFunction = () => {
+    setcount(count - 1)
+}
+const incrementFunction = () => {
+  setcount(count + 1)
+}
     return (
   <>
   <div id="wrapper">
-    <HeaderContent/>
-    <BodyContent>
-      <h3>Home page</h3>
-    </BodyContent>
+   <button onClick={handleClick}>Click</button>
+<p>{num1}</p>
 
- 
+<p>Counter</p>
+<button onClick={decrementFunction}>-</button> 
+<button onClick={incrementFunction}>+</button>
+<p>{count}</p>
     </div>
       
     </>
