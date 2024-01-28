205ff3fb6ff9e981016128d91856dc56026d646f
Sasindumarasinghe1221 <sasindumarasinghe74@gmail.com>
2024-01-28 23:18:38 +0530

    other

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,7 @@ import './App.css';
 
 function App() {
 
-  function sum (...somenumbers){
-    let total = 0;
-    somenumbers.forEach(somenumber => {
-      total = total + somenumber;
-    });
-    return total;
-
-  }
-  console.log(sum(1,2,3,4,5,6,7,8))
-  
-  function mybio(name,channel,...rest){
-    console.log(name,channel,rest)
-
-  }
-  mybio("sasindu","facebook","a","b")
+//
 return (
   <>
   
